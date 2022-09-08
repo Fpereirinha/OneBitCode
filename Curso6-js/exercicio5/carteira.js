ce3c151caa85eb1986bbdc02ad174d9268ecec56
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-09-08 15:34:58 -0300

    new

---
+++ Curso6-js/exercicio5/carteira.js
@@ -0,0 +1,24 @@
+let saldo = parseFloat(prompt("Qual o saldo inicial ?"));
+let resp;
+let value;
+
+do {
+  resp = prompt(`Oque deseja fazer ?\n
+    1- Adicionar
+    2- Subtrair
+    3- Sair`);
+  if (resp == 1) {
+    value = parseFloat(prompt("Qual valor a ser adicionado ?"));
+    saldo += value;
+    alert(`Saldo atual: ${saldo}`);
+  } else if (resp == 2) {
+    value = parseFloat(prompt("Qual valor a ser subtraido ?"));
+    if (value <= saldo) {
+      saldo -= value;
+      alert(`Saldo atual: ${saldo}`);
+    } else {
+      alert("Não foi possivel subtrair, o saldo é menor do que o valor.");
+    }
+  }
+} while (resp != 3);
+alert(`Programa encerrado, valor atual do saldo ${saldo}`);
---
+++ Curso6-js/exercicio5/principal.js
@@ -0,0 +1,14 @@
+let resposta;
+while (true) {
+    resp = prompt(`Qual a opção desejada ?\n
+    1 - xxx\n
+    2 - xxx\n
+    3 - xxx\n
+    4 - xxx\n
+    5 - encerrar`)
+    if (resp == 5){
+        break
+    }
+    alert(`Opção escolhida ${resp}`)
+}
+alert("Programa encerrado")
