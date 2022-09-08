a254bcdedc40789c4e7ad33d20af546a90d1a4ab
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-09-08 15:57:33 -0300

    update

--- Curso6-js/exercicio5/principal.js
+++ Curso6-js/exercicio5/principal.js
@@ -1,14 +1,11 @@
 let resposta;
-while (true) {
-    resp = prompt(`Qual a opção desejada ?\n
+do {
+  resp = prompt(`Qual a opção desejada ?\n
     1 - xxx\n
     2 - xxx\n
     3 - xxx\n
     4 - xxx\n
-    5 - encerrar`)
-    if (resp == 5){
-        break
-    }
-    alert(`Opção escolhida ${resp}`)
-}
-alert("Programa encerrado")
+    5 - encerrar`);
+  alert(`Opção escolhida ${resp}`);
+} while (resp != 5);
+alert("Programa encerrado");
