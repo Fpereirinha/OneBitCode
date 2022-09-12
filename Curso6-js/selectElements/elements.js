c37374aaec5fae5eebed6d0acc7f63021bbdb33f
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-09-12 15:49:25 -0300

    new

---
+++ Curso6-js/selectElements/elements.js
@@ -0,0 +1,6 @@
+function show() {
+	//let element = document.getElementsByName("phone");
+	//let elements = document.querySelectorAll('div#phones input[name = "phone"]');
+	let elements = document.getElementsByTagName("input");
+	console.log(elements);
+}
