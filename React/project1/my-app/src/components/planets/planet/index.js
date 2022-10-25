8aec933abefb56c66c7657f247442200edb16a51
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-10-25 16:36:33 -0300

    props

--- React/project1/my-app/src/components/planets/planet/index.js
+++ React/project1/my-app/src/components/planets/planet/index.js
@@ -1,4 +1,3 @@
-import { Fragment } from "react";
 import "./index.sass";
 import GrayImg from "../../shared/gray-img";
 export default function Planet() {
@@ -10,7 +9,7 @@ export default function Planet() {
 					Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem
 					IpsumLorem IpsumLorem Ipsum
 				</p>
-				<GrayImg />
+				<GrayImg img_url="https://st2.depositphotos.com/4164031/6914/i/450/depositphotos_69145633-stock-photo-flag-of-brazil.jpg" />
 			</div>
 		</div>
 	);
