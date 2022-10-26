a2988386c9c3414b21ed8b76f9bd9a3805ae5fab
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-10-26 13:58:05 -0300

    new

--- React/project1/my-app/src/components/shared/descriptionwlink/index.js
+++ React/project1/my-app/src/components/shared/descriptionwlink/index.js
@@ -4,7 +4,7 @@ export default function Description(props) {
 	return (
 		<Fragment>
 			<p>{props.Description}</p>
-			<a href={props.link} target="_blank">
+			<a href={props.link} target="_blank" rel="noreferrer">
 				Link
 			</a>
 		</Fragment>
